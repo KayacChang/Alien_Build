1d21cb27a3dc0da6b3d16f153238a2ed02c276f6
kayacchang <egg734631@gmail.com>
2020-08-01 17:37:21 +0800

    git subrepo clone --force https://gitlab.fbk168.com/gamedevjp/alien/client.git
    subrepo:
      subdir:   "client"
      merged:   "644f0d6"
    upstream:
      origin:   "https://gitlab.fbk168.com/gamedevjp/alien/client.git"
      branch:   "master"
      commit:   "644f0d6"
    git-subrepo:
      version:  "0.4.1"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "a04d8c2"

--- client/src/system/application.js
+++ client/src/system/application.js
@@ -1,30 +1,55 @@
-import './styles/App.scss';
+import './styles/App.scss'
 
-import {Application} from 'pixi.js';
-import EventEmitter from 'eventemitter3';
-import {Sound} from './modules/sound';
-import {Resource} from './modules/resource';
-import {resize} from './modules/screen';
-import {User} from './user';
+import {Application} from 'pixi.js'
+import EventEmitter from 'eventemitter3'
+import {Sound} from './modules/sound'
+import {Resource} from './modules/resource'
+import {User} from './user'
+import {isMobile} from './modules/device'
 
-const {defineProperties, assign, freeze} = Object;
+const {defineProperties, assign, freeze} = Object
 
-export default (function() {
+function getQuality () {
+    return new URL(location).searchParams.get('quality')
+}
+
+function getScale (quality) {
+    if (quality === 'high') {
+        return 1
+    }
+    if (quality === 'low') {
+        return 0.5
+    }
+
+    return isMobile() ? 0.5 : 1
+}
+
+function getSize (quality) {
+    return {
+        width: 1920 * getScale(quality),
+        height: 1080 * getScale(quality),
+    }
+}
+
+export default (function () {
+    const quality = getQuality()
+
     const app = new Application({
-        resolution: devicePixelRatio || 1,
-        antialias: true,
-    });
+        ...getSize(quality),
+    })
+
+    localStorage.setItem('quality', getQuality())
 
     //  Resource
-    const resource = Resource(app);
-    const user = User(app);
+    const resource = Resource(app)
+    const user = User(app)
     //  Sound
-    const sound = Sound(app);
+    const sound = Sound(app)
 
-    let translate = undefined;
-    let service = undefined;
-    let control = undefined;
-    let alert = undefined;
+    let translate = undefined
+    let service = undefined
+    let control = undefined
+    let alert = undefined
 
     //  Modules
     defineProperties(app, {
@@ -53,36 +78,33 @@ export default (function() {
             get: () => translate,
             set: (translateFunc) => (translate = translateFunc),
         },
-    });
+    })
 
     //  EventCore
-    const eventCore = new EventEmitter();
+    const eventCore = new EventEmitter()
 
     //  Functions
     assign(app, {
         //  EventEmitter ==================
-        on(event, listener) {
-            eventCore.on(event, listener);
+        on (event, listener) {
+            eventCore.on(event, listener)
         },
-        once(event, listener) {
-            eventCore.once(event, listener);
+        once (event, listener) {
+            eventCore.once(event, listener)
         },
-        off(event, listener) {
-            eventCore.off(event, listener);
+        off (event, listener) {
+            eventCore.off(event, listener)
         },
-        emit(event, ...args) {
-            eventCore.emit(event, ...args);
+        emit (event, ...args) {
+            eventCore.emit(event, ...args)
         },
         //  Screen Management ==================
-        resize() {
-            resize(app);
-            app.emit('resize');
+        resize () {
+            app.stage.children.forEach((scene) => {
+                scene.scale.set(getScale(quality))
+            })
         },
-    });
+    })
 
-    //  Event Binding
-    global.addEventListener('resize', app.resize);
-    global.addEventListener('orientationchange', app.resize);
-
-    return freeze(app);
-})();
+    return freeze(app)
+})()
